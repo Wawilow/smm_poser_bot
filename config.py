b2af47c6cd3b57b5562a5fb7ca5266a27a418f3d
Admin <ujif987@mail.ru>
2022-01-27 12:39:25 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,28 @@
+"""Main info"""
+bot_token = 'token'
+schedule_table = [[4, 11], [7, 11], [10, 4], [14, 13], [16, 42], [18, 34], [20, 11], [22, 17]]
+
+bot_group_id = 1
+album_id = 1
+
+"""
+Info for unittests
+"""
+test_user_id = 1
+test_fold = 'users/test_user_id'
+
+
+
+"""
+Project info
+"""
+project_name = 'Auto-posting Bot'
+
+"""ps"""
+
+"""
+to get your token go to link
+https://oauth.vk.com/oauth/authorize?client_id=8035251&scope=photos,audio,video,docs,notes,pages,status,offers,questions,wall,groups,email,notifications,stats,ads,offline,docs,pages,stats,notifications&response_type=token&__q_hash=69940e59922728d45b3bca4d29ab1dc9
+or
+https://vkhost.github.io/
+"""
